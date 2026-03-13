fa7a2065b018960e5b646b264b061524ae399c66
Jesús Urbiñez <Jurbinez@unab.edu.co>
2026-03-13 01:07:08 -0500

    Calculadora: maquetacion, mutabilidad y implementacion de teclado numerico

--- app/src/main/java/me/jesusurbinez/materialdesing/StateTextField.kt
+++ app/src/main/java/me/jesusurbinez/materialdesing/StateTextField.kt
@@ -5,10 +5,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun StateTextField() {
+
+    var inputUser by remember { mutableStateOf(value = "Jesus") }
 
     Scaffold() { innerPading ->
         Column(
@@ -16,10 +23,12 @@ fun StateTextField() {
                 .padding(innerPading)
         ) {
             TextField(
-                value = "",
-                onValueChange = { },
+                value = inputUser,
+                onValueChange = { it -> inputUser = it },
                 label = { Text("Escribe lo que quieras") }
             )
+            Text(fontSize = 35.sp, text = "El usuario escribio:")
+            Text(fontSize = 35.sp, text = inputUser)
         }
 
     }
