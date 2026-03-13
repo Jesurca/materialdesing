eb4269ec55644c1fc993c2c7ad6886123ca181e3
Jesús Urbiñez <Jurbinez@unab.edu.co>
2026-03-13 01:19:23 -0500

    Calculadora: implementacion de redondeo

--- app/src/main/java/me/jesusurbinez/materialdesing/CalculatorIva.kt
+++ app/src/main/java/me/jesusurbinez/materialdesing/CalculatorIva.kt
@@ -1,20 +1,22 @@
 package me.jesusurbinez.materialdesing
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -28,12 +30,13 @@ fun CalculatorIva() {
 
     var inputAmount by remember { mutableStateOf("") }
     var inputPercent by remember { mutableStateOf("") }
+    var switchRounded by remember { mutableStateOf(false) }
 
     val amount = inputAmount.toDoubleOrNull() ?: 0.0
     val percent = inputPercent.toDoubleOrNull() ?: 0.0
 
     val result = amount * percent / 100
-
+    val resultRounded = kotlin.math.ceil(result)
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier
@@ -65,9 +68,20 @@ fun CalculatorIva() {
                 label = { Text("Porcentaje") },
                 modifier = Modifier.fillMaxWidth()
             )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Redondear")
+                Switch(
+                    checked = switchRounded,
+                    onCheckedChange = { switchRounded = it }
+                )
+            }
 
             Text(
-                text = "Resultado: $ $result",
+                text = "Resultado: $ ${if (switchRounded) resultRounded else result}",
                 modifier = Modifier.padding(bottom = 16.dp, top = 20.dp),
                 fontSize = 25.sp
             )
